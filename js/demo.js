85c043d18fa0977fba86779d8ee9d2a56cf749f0
liaohui1080 <liaohui1080@tianliaohui.com>
2017-01-05 19:09:58 +0800

    简历

---
+++ js/demo.js
@@ -0,0 +1,14 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#1c71a7',
+    lineColor: '#1c6ca3'
+  });
+  // $('.intro').css({
+  //   'margin-top': -($('.intro').height() / 2)
+  // });
+});
